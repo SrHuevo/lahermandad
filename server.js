4818425e25ef944d961d595a0eefa87f14a8d45c
SrHuevo <dgaroz@lan.krama.es>
2016-09-04 16:04:28 +0200

    configure port

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 var http = require('http');
 var server = http.createServer();
 
+var port = process.env.PORT || 8080;
+
 function handler(req, resp) {
 	resp.writeHead(301,
-	  {Location: 'http://hermandadurjc.wixsite.com/paginaoficial/proximas-actividades'}
+	  {Location: 'http://hermandadurjc.wixsite.com/paginaoficial'}
 	);
 	resp.end();
 }
 
 server.on('request', handler);
 
-server.listen(8080);
+server.listen(port);
