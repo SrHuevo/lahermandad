4ddda5e9b82b5e3fcf61d46d30cefd9408d55e7b
SrHuevo <dgaroz@lan.krama.es>
2016-09-07 01:16:23 +0200

    varios redirects

--- server.js
+++ server.js
@@ -4,17 +4,24 @@ var server = http.createServer();
 var port = process.env.PORT || 8080;
 
 function handler(req, resp) {
+	var redirect = getRedirect(req.url);
 	resp.writeHead(301,
-	  {Location: 'http://hermandadurjc.wixsite.com/paginaoficial'}
+	  {Location: redirect}
 	);
 	resp.end();
 }
-
+http://hermandadurjc.wixsite.com/paginaoficial/que-hacemos
 server.on('request', handler);
 
 server.listen(port);
 
-
+function getRedirect(url) {
+	if(url === '/apocalipsiszombie'){
+		return 'http://hermandadurjc.wixsite.com/paginaoficial/apcalipsis-zombie';
+	} else {
+		return 'http://hermandadurjc.wixsite.com/paginaoficial'
+	}
+}
 
 var http = require('http');
 var options = [{
@@ -26,7 +33,6 @@ setInterval(function(){
 	options.forEach(function(e){
 		http.get(e, function(res) {
 		  console.log('STATUS: ' + res.statusCode);
-		  console.log('HEADERS: ' + JSON.stringify(res.headers));
 		});
 	});
-},5000);
+},60000 * Math.random() * 10);
