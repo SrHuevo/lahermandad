898b518c3f7239478441c21efc4588ee7cd0e15a
SrHuevo <dgaroz@lan.krama.es>
2016-09-08 13:59:51 +0200

    express migration

---
+++ controller/redirect.js
@@ -0,0 +1,20 @@
+function get(req, resp) {
+	console.log('redirect: get')
+	var redirect = getRedirect(req.url);
+	resp.writeHead(301,
+	  {Location: redirect}
+	);
+	resp.end();
+}
+
+function getRedirect(url) {
+	if(url === '/apocalipsiszombie'){
+		return 'http://hermandadurjc.wixsite.com/paginaoficial/apcalipsis-zombie';
+	} else if(url) {
+		return 'http://hermandadurjc.wixsite.com/paginaoficial/' + url;
+	} else {
+		return 'http://hermandadurjc.wixsite.com/paginaoficial';
+	}
+}
+
+exports.get = get;
--- server.js
+++ server.js
@@ -1,38 +1,32 @@
-var http = require('http');
-var server = http.createServer();
+var express = require('express');
+var bodyParser = require('body-parser');
+var sellTikets = require('./controller/sell-tikets');
+var redirect = require('./controller/redirect');
 
+var app = express();
 var port = process.env.PORT || 8080;
 
-function handler(req, resp) {
-	var redirect = getRedirect(req.url);
-	resp.writeHead(301,
-	  {Location: redirect}
-	);
-	resp.end();
-}
-http://hermandadurjc.wixsite.com/paginaoficial/que-hacemos
-server.on('request', handler);
+app.get('/api/venta', sellTikets.get);
+app.post('/api/venta', sellTikets.post);
+app.get('/*', redirect.get);
 
-server.listen(port);
-
-function getRedirect(url) {
-	if(url === '/apocalipsiszombie'){
-		return 'http://hermandadurjc.wixsite.com/paginaoficial/apcalipsis-zombie';
-	} else {
-		return 'http://hermandadurjc.wixsite.com/paginaoficial'
-	}
-}
+app.listen(port);
 
 var http = require('http');
 var options = [{
   host: 'lahermandad.herokuapp.com'
 }];
 
-
+var min = 60000;
+var max = 600000;
+// and the formula is:
+var random = function(){
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 setInterval(function(){
 	options.forEach(function(e){
 		http.get(e, function(res) {
 		  console.log('STATUS: ' + res.statusCode);
 		});
 	});
-},60000 * Math.random() * 10);
+}, random());
