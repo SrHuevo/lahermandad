80124fa2ab03ae13be31083395e13d65f5ffdcaf
srHuevo <garoz.daniel2gmail.com>
2016-09-27 18:06:06 +0200

    onanismo.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('./util/onanismo');
 var express = require('express');
 var redirect = require('./controller/redirect');
 
@@ -7,26 +8,3 @@ var port = process.env.PORT || 8080;
 app.get('/*', redirect.get);
 
 app.listen(port);
-
-
-
-
-//Heroku free no sleep
-var http = require('http');
-var options = [{
-  host: 'lahermandad.herokuapp.com'
-}];
-
-var min = 60000;
-var max = 600000;
-// and the formula is:
-var random = function(){
-	return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-setInterval(function(){
-	options.forEach(function(e){
-		http.get(e, function(res) {
-		  console.log('STATUS: ' + res.statusCode);
-		});
-	});
-}, random());
